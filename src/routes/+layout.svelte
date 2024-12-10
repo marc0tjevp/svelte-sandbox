3782aa8223f9bb80958573a90664fef8216ca075
Marco van Poortvliet <marc0tjevp@gmail.com>
2024-12-10 12:22:52 +0100

    Simple setup

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,16 @@
+<script>
+	import { page } from '$app/stores';
+</script>
+<nav>
+	<ul>
+		<li class={$page?.url?.pathname === '/' ? 'active' : ''}><a href="/">Home</a></li>
+		<li class={$page?.url?.pathname === '/about' ? 'active' : ''}><a href="/about">About</a></li>
+		<li class={$page?.url?.pathname === '/contact' ? 'active' : ''}>
+			<a href="/contact">Contact</a>
+		</li>
+	</ul>
+</nav>
+
+<main>
+	<slot />
+</main>
